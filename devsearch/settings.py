fab40b5e2b3200b233fb370c7cd7d8bb093f0baf
vivek vasoya <69588474+vvasoya35@users.noreply.github.com>
2022-10-24 16:27:52 +0530

    first commit

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fuh8aerj@81@qp9(epj+_rm!09s7d^lf$dk=-)$$t0@_*+w4%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
+SESSION_COOKIE_SECURE=True
+CSRF_COOKIE_SECURE=True
+SECURE_SSL_REDIRECT = False
 # ALLOWED_HOSTS = ['https://vdevsearch.herokuapp.com/','vdevsearch.herokuapp.com/','vdevsearch.herokuapp.com','http://127.0.0.1/','http://127.0.0.1:8000/','127.0.0.1:8000','127.0.0.1']
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
