824f3093f92a6c06ee52c2cd47fe792509badd0a
vivek vasoya <69588474+vvasoya35@users.noreply.github.com>
2022-10-25 18:42:41 +0530

    one

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,8 +12,8 @@ urlpatterns = [
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
     path('api/', include('api.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
 
     path('reset_password',auth_views.PasswordResetView.as_view(template_name='reset_password.html'),name='reset_password'),
     
