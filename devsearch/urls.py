2984f770d9018555fdffef54008b9afc6f574243
vivek vasoya <69588474+vvasoya35@users.noreply.github.com>
2022-10-25 18:45:43 +0530

    second

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
+# from django.views.static import serve
 # from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
